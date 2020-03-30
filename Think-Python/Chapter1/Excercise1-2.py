8273d21650369517679b687c693f3399ce13fe24
Vr3n <vickyspatel@gmail.com>
2020-03-30 23:44:40 +0530

    Completed Excercise1

---
+++ Think-Python/Chapter1/Excercise1-2.py
@@ -0,0 +1,12 @@
+# 1. How many seconds are there in 42 minutes 42 seconds.
+
+print(f"There are {42 * 60 + 42} seconds in 42mins and 42secs")
+
+
+# 2. How many miles and there in 10 kilometers? Hint: there are 1.61 miles in a Kilometer.
+
+print(f"There are {10 * 0.62} miles in 10 kilometers")
+
+# 3. If you run a 10km race in 42 minutes 42 seconds. what is your average price (time per mile in minutes and seconds)? what is your average speed in miles per hour.
+
+print(f'Average speed is {10 * 0.62 / 42 * 60 + 42 // 3600} mph')
