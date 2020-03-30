65c6df5901f312d93bec5cd099a144aba2b7eded
Vr3n <vickyspatel@gmail.com>
2020-03-31 01:00:50 +0530

    Completed 2 excericise of Chapter 2

---
+++ Think-Python/Chapter2/Ex2.1.py
@@ -0,0 +1,3 @@
+# 1. The volume of a sphere with radius r is 4/3 * pi * r^3. What is the volume of a sphere with radius 5.
+
+print(f'The volume of sphere is {4.0/3 * 3.14 * 5 ** 3}')
---
+++ Think-Python/Chapter2/Ex2.2.py
@@ -0,0 +1,34 @@
+# 2. Suppose the cover price of a book is $26.95, but bookstores get a 40% discount. Shipping costs $3 for the first copy and 75 cents for each additional copy. what is the total wholesale cost of 60 copies.
+
+cover_price = 24.95
+discount_percentage = 40
+number_of_books = 60
+
+def ship_cost(n):
+	"""
+	Function to calculate shipping cost per book.
+	"""
+	if n == 1:
+		return (n * 3)  # number of books * cost of shipping.
+	else:
+		return (3 + (n - 1) * 0.75) # Additional book copy costs $3 and 75cents.
+
+
+def discount(price, d):
+	"""
+	Function to calculate discount.
+	"""
+	return (price - (price * (d / 100))) # Discount of 40%.
+
+
+def wholesale_price(price, number_of_books):
+	"""
+	Function to Calculate Wholesale price.
+	"""
+	return ((price * number_of_books) + ship_cost(number_of_books))
+
+d = discount(cover_price, discount_percentage)
+wp = wholesale_price(d, number_of_books)
+
+print(f" The discounted price is {d}")
+print(f"The wholesale price of the book is {wp}")
